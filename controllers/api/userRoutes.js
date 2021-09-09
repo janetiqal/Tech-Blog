5d8f2774c859585b09448f6e0a2c92fdd5881660
Janet Iqal <j.iqal35@gmail.com>
2021-09-09 16:28:56 -0500

    initial file structure setup

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// const { User } = require('../../models');
+
+router.post('/login', async (req,res)=>{
+    // TO DO:
+    //Write user login post route after creating the USER MODEL 
+})
+
+router.post('/logout', async (req,res)=>{
+    // TO DO:
+    //Write user logout post route after creating the USER MODEL 
+})
