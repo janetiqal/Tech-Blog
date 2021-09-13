2c1a72a0d522868500e3fdbee0d2a82279f1ea3c
Janet Iqal <j.iqal35@gmail.com>
2021-09-13 16:42:58 -0500

    added get routes for all users and user by id

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,40 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+//USER data is returned w name, email 
 router.get('/', (req,res)=>{
-
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
+    .then(userdata => res.status(200).json(userdata))
+    .catch(err=>{
+        res.status(500).json(err)
+    })
 })
-
-
+//USER data is returned w name, email and Posts created by this user
+router.get('/:id', async (req,res)=>{
+   try{
+    const oneUser= await User.findByPk(req.params.id,{
+        where:{
+            id:req.params.id
+        },
+        attributes: {
+            exclude: ['password'],
+        },
+        include:{
+            model: Post,
+            attributes: ['title']
+        },
+    })
+    if(!oneUser){
+        res.status(404).json({message:"No user found with this ID"})
+    }
+    res.status(200).json(oneUser)
+   }
+   catch(err){
+       res.status(500).json(err)
+   }
+})
 
 
 
--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const router= require('express').Router();
 
 //The user must be logged in--but they'll be able to get all there posts , edit them? but also create a post
-
+//THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
 module.exports= router;
