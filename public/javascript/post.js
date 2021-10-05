eb1cbf7dcb5277cc8b32cf665888e4955b23e554
Janet Iqal <j.iqal35@gmail.com>
2021-10-05 10:54:37 -0500

    fixed the queryselector for edit post functionality

--- public/javascript/post.js
+++ public/javascript/post.js
@@ -4,52 +4,48 @@ console.log("post js connected")
 const editPostHandler = async (event) => {
     event.preventDefault();
     console.log(event.target)
-    //error here: not selecting the correct values
-        //if i used queryselectorAll they return undefined..
-        //if left as is, they take the value of the first post and update the current post w the original posts title and body..
-    // const title = document.querySelector('.newTitle').value
+
     const postID = event.target.getAttribute('data-id')
-    const title = document.querySelector('.newTitle').value
-    // const title = document.querySelector('textarea[name="newTitle"]').value
-    const post_body = document.querySelector('.newBody').value
+    const title = document.querySelector(`#title${postID}`).value
+    const post_body = document.querySelector(`#body${postID}`).value
+
     console.log("new post title:", title, "post id:", postID, "post body:", post_body)
 
-    // if (title || post_body) {
-        const response = await fetch(`api/post/${postID}`, {
-            method: 'PUT',
+
+    const response = await fetch(`api/post/${postID}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title: title, post_body: post_body }),
+        headers: { 'Content-Type': 'application/json' },
+    })
+    if (response.ok) {
+        document.location.reload('/dashboard')
+    }
+}
+
+const updateBtns = document.querySelectorAll('.update-post');
+[...updateBtns].forEach(updateBtn => updateBtn.addEventListener('click', editPostHandler))
+
+// create post js
+const createPostHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector('.newPostTitle').value.trim()
+    const post_body = document.querySelector('.postBody').value.trim()
+
+    console.log(title, post_body)
+    if (title && post_body) {
+        const response = await fetch('api/post', {
+            method: 'POST',
             body: JSON.stringify({ title: title, post_body: post_body }),
             headers: { 'Content-Type': 'application/json' },
         })
         if (response.ok) {
             document.location.reload('/dashboard')
         }
-    // }
-}
-//TO DO:  the value of post body is set as  the first post's body.. not updating but data-id is updating.
-const updateBtns = document.querySelectorAll('.update-post');
-[...updateBtns].forEach(updateBtn => updateBtn.addEventListener('click', editPostHandler))
-
-// create post js
-const createPostHandler = async (event)=>{
-    event.preventDefault();
-    const title = document.querySelector('.newPostTitle').value.trim()
-    const post_body = document.querySelector('.postBody').value.trim()
-    
-    console.log(title, post_body)
-    if(title && post_body){
-        const response = await fetch('api/post',{
-            method:'POST',
-            body:JSON.stringify({title:title,post_body:post_body}),
-            headers: { 'Content-Type': 'application/json' },
-        })
-        if (response.ok) {
-            document.location.reload('/dashboard')
+        else {
+            alert(response.statusText)
+            console.log(response.statusText)
         }
-      else{
-       alert(response.statusText)
-        console.log(response.statusText)
     }
-}
 }
 const newPostBtn = document.querySelector('.new-post').addEventListener('click', createPostHandler)
 
